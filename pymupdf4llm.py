cb7b55715b70cdb201f6a573b9197cebe39643a2
Yiyu Wang <wangyiyu980@gmail.com>
2024-09-19 11:58:02 -0400

    update

--- pymupdf4llm.py
+++
@@ -1,4 +0,0 @@
-import pymupdf4llm
-import pathlib
-md_text = pymupdf4llm.to_markdown(r'C:\Users\wangy\Desktop\test_table.pdf')
-pathlib.Path(r'C:\Users\wangy\Desktop\output.md').write_bytes(md_text.encode())
